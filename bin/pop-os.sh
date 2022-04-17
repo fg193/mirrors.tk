1f2775f6cc950fbeead93046703291b42a9a2bee
Arnie97 <arnie97@gmail.com>
2022-04-17 16:25:13 +0800

    Enable upload path mapping

--- bin/pop-os.sh
+++ bin/pop-os.sh
@@ -3,5 +3,5 @@
 task=pop-os
 
 for dir in release proprietary; do
-	rclone copy -v $task:$dir/ flow:$task/$dir
+	rclone copy -v --http-no-head --ignore-existing --create-empty-src-dirs $task:$dir/ flow:$task/$dir
 done
