8107ff253e3a6e4a0b473e12fe54c503177bd7c8
Arnie97 <arnie97@gmail.com>
2022-04-23 14:05:25 +0800

    Add repo pop-os/staging/master

--- bin/pop-os.sh
+++ bin/pop-os.sh
@@ -4,7 +4,7 @@ task=pop-os
 
 dumb.sh syncing $task 0 ${interval:=24h}
 
-for dir in release proprietary staging-proprietary; do
+for dir in release proprietary staging/master staging-proprietary; do
 	rclone copy $task:$dir/pool/  flow:$task/$dir/pool  -v --transfers=1 --stats-file-name-length=0 --http-no-head --ignore-existing
 	rclone copy $task:$dir/dists/ flow:$task/$dir/dists -v --transfers=1 --stats-file-name-length=0
 done
